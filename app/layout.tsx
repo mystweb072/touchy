a3e7a1fa7040f9fe5c0c3b507eb048344657dfb2
mystweb <mystweb072@gmail.com>
2025-12-25 13:01:42 +0100

    Fix SW 404 error with rename

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   useEffect(() => {
     if ("serviceWorker" in navigator) {
       navigator.serviceWorker
-        .register("/worker-push.js")
+        .register("/push-sw.js")
         .then((reg) => console.log("SW zarejestrowany:", reg))
         .catch((err) => console.error("Błąd rejestracji SW:", err));
     }
